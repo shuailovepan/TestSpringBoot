4323fe7fc81085db7200592abe903bae6498522d
shuailovepan <2457094838@qq.com>
2019-03-24 10:48:11 +0800

    第一次提交代码

---
+++ demo/src/main/java/com/example/demo/controller/TestIdea.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestIdea {
+
+
+
+    public  String TestHello(){
+      return  "李帅十分想念李攀";
+    }
+}
